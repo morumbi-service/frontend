4b57c5b1ecb39b83d000091eec555b7083172048
Giovanna Ribeiro <giovanna_oliveira.ribeiro@live.com>
2021-01-21 16:35:05 -0300

    ADD: Styled-components to the project

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,13 @@
+import {
+  Container
+} from './styles'
+
+function Header() {
+  return (
+    <Container>
+      <h1>Header</h1>
+    </Container>
+  )
+}
+
+export default Header
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,10 @@
+import Header from '../components/Header/index'
+
 export default function Home() {
   return (
     <div>
       <main>
+        <Header />
         <h2>Prepare-se... Algo muito legal está chegando em breve!</h2>
       </main>
     </div>
